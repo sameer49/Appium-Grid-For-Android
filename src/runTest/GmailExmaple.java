232b2ae0693b08337c05a73219eb9f622c1e95ca
sameer49 <sameersawant222@gmail.com>
2015-02-24 14:58:42 +0530

    extended BaseTest and ran test from here

--- src/runTest/GmailExmaple.java
+++ src/runTest/GmailExmaple.java
@@ -1,21 +1,20 @@
 package runTest;
 
-import io.appium.java_client.android.AndroidDriver;
-
+import libs.BaseTest;
 import org.openqa.selenium.By;
 
-public class GmailExmaple {
 
-	AndroidDriver driver;
-	
-	public GmailExmaple(AndroidDriver driver) {
-		this.driver=driver;
+public class GmailExmaple extends BaseTest{
+
+	public GmailExmaple(){
 	}
-
-	public void login()
-	{
-		try
-		{
+	
+	public GmailExmaple(int deviceNum){
+		super(deviceNum);
+	}
+	
+	public void login(){
+		try{
 			driver.get("http://gmail.com/");
 			Thread.sleep(2000);
 			driver.findElement(By.id("Email")).sendKeys("test");
@@ -25,14 +24,34 @@ public class GmailExmaple {
 			driver.findElement(By.id("signIn")).click();
 			Thread.sleep(2000);
 	    }
-	    catch(Exception e)
-	    {
+	    catch(Exception e){
 	    	e.printStackTrace();
 	    }
-	    finally
-	    {
-	    	// close driver
-	    	driver.quit();
+	    finally{
+	    	driver.quit(); 	    	// close driver
 	    }
+	}
+	
+	public void run(){
+		login();
+	}
+	
+	public static void main(String[] args) {
+		// Create object
+		GmailExmaple gmail = new GmailExmaple();
+		
+		// Get connected device count
+		int totalDevices=gmail.deviceCount;
+		
+		// Initialize threads for each connected devices
+		GmailExmaple[] threads = new GmailExmaple[totalDevices];
+		
+		// Create threads for each connected devices
+		for(int i=0;i<totalDevices;i++)
+			threads[i] = new GmailExmaple(i);
+		
+		// Start running execution on each device
+		for(int i=0;i<totalDevices;i++)
+			threads[i].start();
 	}
 }
