5f0f22e49dad007846be443f6060cf35d6579781
sameer49 <sameersawant222@gmail.com>
2015-02-24 15:14:11 +0530

    added android app example

---
+++ src/runTest/CalcExample.java
@@ -0,0 +1,39 @@
+package runTest;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class CalcExample {
+
+	AndroidDriver driver;
+	
+	public CalcExample(AndroidDriver driver) {
+		this.driver=driver;
+	}
+	
+	public void performOperations() {
+		
+		try
+		{
+			driver.findElement(By.id("com.android2.calculator3:id/cling_dismiss")).click();
+			driver.findElement(By.id("com.android2.calculator3:id/digit5")).click();
+			driver.findElement(By.id("com.android2.calculator3:id/plus")).click();
+			driver.findElement(By.id("com.android2.calculator3:id/digit9")).click();
+			driver.findElement(By.id("com.android2.calculator3:id/equal")).click();
+			String num = driver.findElement(By.xpath("//android.widget.EditText[@index=0]")).getText();
+			System.out.println("Result : "+num);
+			Thread.sleep(2000);
+	    }
+	    catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+	    finally
+	    {
+	    	// close driver
+	    	driver.closeApp();
+	    }
+	}
+
+}
